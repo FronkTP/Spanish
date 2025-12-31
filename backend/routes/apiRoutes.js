38cf773a788e289af12b375570957b6e005f0603
FronkTP <96272954+FronkTP@users.noreply.github.com>
2026-01-01 04:11:39 +0700

    add route to update word status

--- backend/routes/apiRoutes.js
+++ backend/routes/apiRoutes.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { getDailyWords } from "../controllers/getDailyWords.js";
+import { updateWordStatus } from "../controllers/updateWordStatus.js";
 
 export const apiRouter = express.Router();
 
 apiRouter.get("/words/daily", getDailyWords);
+apiRouter.post("/words/:word_id/status", updateWordStatus);
