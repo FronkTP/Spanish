cc848cbd5b2ac29a9d7df5620692e464786987d4
FronkTP <96272954+FronkTP@users.noreply.github.com>
2026-01-01 03:49:44 +0700

    change 404 response field to error for consistency

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,9 @@ app.use(cors());
 app.use("/", apiRouter);
 
 app.use((req, res) => {
-  res
-    .status(404)
-    .json({
-      message: "Endpoint not found. Please check the API documentation.",
-    });
+  res.status(404).json({
+    error: "Endpoint not found. Please check the API documentation.",
+  });
 });
 
 app.listen(PORT, () => console.log(`server connected on port ${PORT}`));
