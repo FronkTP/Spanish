003d63d3d86cd6c67ef9ceeae19f78a509daf899
FronkTP <96272954+FronkTP@users.noreply.github.com>
2026-01-14 03:00:17 +0700

    update controller function name in import

--- backend/routes/apiRoutes.js
+++ backend/routes/apiRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { getDailyWords } from "../controllers/getDailyWords.js";
-import { updateWordStatus } from "../controllers/updateWordStatus.js";
+import { wordsController } from "../controllers/wordsController.js";
+import { statusController } from "../controllers/statusController.js";
+import { progressController } from "../controllers/progressController.js";
 
 export const apiRouter = express.Router();
 
-apiRouter.get("/words/daily", getDailyWords);
-apiRouter.post("/words/:word_id/status", updateWordStatus);
+apiRouter.get("/words/daily", wordsController);
+apiRouter.post("/words/:word_id/status", statusController);
+apiRouter.get("/progress", progressController);
