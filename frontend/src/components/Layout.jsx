a2dbdf33822e1ece8e0fb6ed8154c06faa190cde
FronkTP <96272954+FronkTP@users.noreply.github.com>
2026-01-20 20:11:10 +0700

    refactor layout structure for improved responsiveness on other pages

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -3,13 +3,13 @@ import Header from "./Header";
 
 export default function Layout() {
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="min-h-screen bg-background-light">
+      <main className="flex-1 bg-background-light">
         <div className="max-w-7xl mx-auto p-6">
           <Outlet />
         </div>
       </main>
-    </>
+    </div>
   );
 }
