b8ef5afc83d5021d5008f690970524585934e165
FronkTP <96272954+FronkTP@users.noreply.github.com>
2026-01-20 20:10:14 +0700

    style the header and navigation bar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,14 +2,33 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header>
-      <Link to="/">logo</Link>
-      <nav>
-        <Link to="/">Home</Link>
-        {/* <Link to="/all">1000</Link> */}
-        <Link to="/progress">progress</Link>
-        <Link to="/practice">Practice</Link>
-      </nav>
+    <header className="w-full bg-white shadow-sm border-b-2 border-primary">
+      <div className="max-w-7xl mx-auto flex justify-between px-4 py-3">
+        <Link to="/" className="text-2xl text-primary font-bold">
+          Spanish
+        </Link>
+        <nav className="flex items-center gap-6">
+          <Link
+            to="/"
+            className="text-gray-600 hover:text-primary transition-colors"
+          >
+            Home
+          </Link>
+          {/* <Link to="/all">1000</Link> */}
+          <Link
+            to="/progress"
+            className="text-gray-600 hover:text-primary transition-colors"
+          >
+            Progress
+          </Link>
+          <Link
+            to="/practice"
+            className="text-gray-600 hover:text-primary transition-colors"
+          >
+            Practice
+          </Link>
+        </nav>
+      </div>
     </header>
   );
 }
