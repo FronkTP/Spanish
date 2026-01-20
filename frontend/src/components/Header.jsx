bc63cccd1df269dd87edad9631018c84373cb96b
FronkTP <96272954+FronkTP@users.noreply.github.com>
2026-01-20 18:13:22 +0700

    hide table page from the header navigation

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
       <Link to="/">logo</Link>
       <nav>
         <Link to="/">Home</Link>
-        <Link to="/all">1000</Link>
+        {/* <Link to="/all">1000</Link> */}
         <Link to="/progress">progress</Link>
         <Link to="/practice">Practice</Link>
       </nav>
